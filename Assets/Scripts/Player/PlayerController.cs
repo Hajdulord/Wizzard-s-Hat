a4d9bf5c96342a9d29d1d9605863300b28fec19f
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-11-27 14:45:02 +0100

    Basic setup.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace WizzardsHat.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody2D = null!;
+        [SerializeField] private float _jumpForce = 8f;
+
+        public void OnSpaceHit(InputAction.CallbackContext callback)
+        {
+            if (callback.started)
+            {
+                _rigidbody2D.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
